8a9a8ac899de132327ff6e357e880cdc94e58300
Aditya Patra <adityabpatra@gmail.com>
2025-09-13 12:16:20 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void enterArrayData(double* headptr,double SIZE);
-void outputArrayData(double* headptr,double SIZE);
-double sumArray(double* headptr,double SIZE);
+void enterArrayData(double* headptr,int SIZE);
+void outputArrayData(double* headptr,int SIZE);
+double sumArray(double* headptr,int SIZE);
 
 int main() {
     const int SIZE = 5;
     double* headptr = new double[SIZE];
     double sum;
+    
     enterArrayData(headptr, SIZE);
     outputArrayData(headptr, SIZE);
-    cout << "Sum of values: " << sumArray(headptr, SIZE);
+    cout << "Sum of values: " << sumArray(headptr, SIZE) << endl;
+    
     delete [] headptr;
 }
 
-void enterArrayData(double* headptr,double SIZE) {
+void enterArrayData(double* headptr,int SIZE) {
     cout << "Data entry for the array: \n";
     for(int i = 0; i < SIZE; i++) {
         cout << "Element " << (i+1) << ": ";
@@ -24,7 +26,7 @@ void enterArrayData(double* headptr,double SIZE) {
     cout << "Data entry complete\n";
 }
 
-void outputArrayData(double* headptr,double SIZE) {
+void outputArrayData(double* headptr,int SIZE) {
     cout << "Outputting Array Elements: ";
     for(int i = 0; i < SIZE; i++) {
         cout << *(headptr+i) << " ";
@@ -32,7 +34,7 @@ void outputArrayData(double* headptr,double SIZE) {
     cout << endl;
 }
 
-double sumArray(double* headptr,double SIZE) {
+double sumArray(double* headptr,int SIZE) {
     double total = 0.0;
     for (int i = 0; i < SIZE; i++) {
         total += *(headptr+i);
